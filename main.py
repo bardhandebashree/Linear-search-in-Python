298f777d43262479a16996046e51eeca48dbf95b
bardhandebashree <33868917+bardhandebashree@users.noreply.github.com>
2022-03-21 21:29:40 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def linear_search(arr, k):
+    flag = 0
+    c = 0
+    l = [0 for i in range(5)]
+
+
+    length = len(arr)
+    for i in range(0, length - 1, 1):
+
+        print("wwwwwwwwwwwwwwwwwwwwww")
+        if k == arr[i]:
+            flag = 1
+            print(c)
+            l[c] = i
+            c = c + 1
+    if flag == 1:
+        print("Found in")
+        print(l)
+    else:
+        print("Not Found")
+
+
+
+    # Use a breakpoint in the code line below to debug your script.
+    # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    arr = [3, 45, 67, 67, 67]
+    print(arr)
+    k = int(input(':input number to be searched'))
+    linear_search(arr, k)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
